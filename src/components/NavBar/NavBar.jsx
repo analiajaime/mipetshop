0c6ae1ad5509e0b2892aa1a3542fe5fd4103f1af
Analia Jaime <anjaime@yahoo.com.ar>
2023-12-02 15:52:25 -0300

    primero pasos

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <header>
+    <h1>Mi PetShop Online</h1> 
+    <nav>  
+        <ul>
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Contacto</li>
+            <li>Carrito</li>
+        </ul>
+    </nav>
+    </header>
+  )
+}
+
+export default NavBar
