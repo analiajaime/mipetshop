19da96f992dae2f79a7384968514afacdb42d5e6
Analia Jaime <anjaime@yahoo.com.ar>
2023-12-23 01:46:19 -0300

    y vamos por más

--- src/componentes/Item/Item.jsx
+++ src/componentes/Item/Item.jsx
@@ -1,17 +1,16 @@
-import './Item.css'
-import React from 'react'
+import './Item.css';
+import { Link } from 'react-router-dom';
 
-
-
-const Item = ({id, nombre, precio, img, idCat}) => {
+const Item = ({id, nombre, precio, img, idcat}) => {
   return (
-    <div>
+    <div className='cardProducto'>
         <img src={img} alt={nombre} />
         <h3>Nombre: {nombre} </h3>
         <p>Precio: {precio} </p>
         <p>ID: {id} </p>
-        <br />
-        <button> Ver Detalles </button>
+        <p>Categoria:{idcat}</p>
+        <p>Stock: 10</p>
+        <Link className='btn' to={`/item/${id}`}> Ver Detalles </Link>
     </div>
   )
 }
